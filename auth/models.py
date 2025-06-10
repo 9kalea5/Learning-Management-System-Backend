709afbf976abba4368d523f1550936d74865cb9a
kaleab <alebachewkaleab@gmail.com>
2025-06-10 21:05:10 +0300

    User Model

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(unique=True)
+    otp = models.CharField(unique=True, max_length=150)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS  = ['username']
+    
+    def __str__(self):
+        return self.email
