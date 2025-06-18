50ed76a6cb4f7d9e05cc8e8fda3af21388944248
kaleab <alebachewkaleab@gmail.com>
2025-06-18 10:50:47 +0300

    Profile Seriailzer

--- core/serializer.py
+++ core/serializer.py
@@ -5,3 +5,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = "__all__"
+        
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = "__all__"
