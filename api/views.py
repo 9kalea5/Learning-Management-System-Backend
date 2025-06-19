64ee06ecf2a3b636cb43cba783b9b0f7ea82ab1d
kaleab <alebachewkaleab@gmail.com>
2025-06-19 17:31:23 +0300

    Generate OTP

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 from core.models import CustomUser, Profile
 from api import serializer as api_serializer
@@ -11,3 +12,16 @@ class MyTokenObtainPairView(TokenObtainPairView):
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = [AllowAny]
+    
+def generate_otp(length=7):
+    otp = ''.join([str(random.randint(0,9)) for _ in range(length)])
+    return otp
+    
+class PasswordResetEmailVerifyApiView(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.UserSerializer
+    
+    def get_object(self):
+        email = self.kwargs['email']
+        
+        user = CustomUser.objects.filter(email=email).first()
