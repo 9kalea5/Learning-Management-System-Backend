f37db4f543feae8005697b5a0414452f43b7a7fa
kaleab <alebachewkaleab@gmail.com>
2025-06-19 12:01:40 +0300

    Register View

--- api/views.py
+++ api/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from core.models import CustomUser, Profile
 from api import serializer as api_serializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = api_serializer.MyTokenPairSerializer
+    
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [AllowAny]
