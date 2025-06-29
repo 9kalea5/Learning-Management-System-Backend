50a17a57173898828877238d563e9156f61bb886
kaleab <alebachewkaleab@gmail.com>
2025-06-29 07:24:25 +0300

    Category Serializer

--- api/serializer.py
+++ api/serializer.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from api import models as api_models
 from core.models import Profile, CustomUser
 from rest_framework_simplejwt .serializers import TokenObtainPairSerializer
 
@@ -50,3 +51,8 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = "__all__"
+        
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = api_models.Category
+        fields = '__all__'
