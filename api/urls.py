ba595db0ec3a5ea9b5fa6c59dc0fca6a1af36d3a
kaleab <alebachewkaleab@gmail.com>
2025-07-01 08:20:09 +0300

    Category urls

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("user/register/", api_views.RegisterView.as_view()),
     path("user/password-reset-email/<email>/", api_views.PasswordResetEmailVerifyApiView.as_view()),
     path("user/password-change/", api_views.PasswordChangeAPIView.as_view()),
+
+    path("course/category/", api_views.CategoryListAPIView.as_view())
 ]
