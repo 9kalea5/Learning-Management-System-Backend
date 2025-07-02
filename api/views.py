b749c2e722e0f4498667b106aba3d7f8b1293295
kaleab <alebachewkaleab@gmail.com>
2025-07-02 20:01:21 +0300

    Cart destroy View

--- api/views.py
+++ api/views.py
@@ -154,3 +154,13 @@ class CartListAPIView(generics.ListAPIView):
         cart_id = self.kwargs['cart_id']
         queryset = api_models.Cart.objects.filter(cart_id=cart_id)
         return queryset
+    
+class CartItemDeleteAPIView(generics.DestroyAPIView):
+    serializer_class = api_serializer.CartSerializer
+    permission_classes = [AllowAny]
+    
+    def get_object(self):
+        cart_id = self.kwargs['cart_id']
+        item_id = self.kwargs['item_id']
+        
+        return api_models.Cart.objects.filter(cart_id=cart_id, item_id=item_id).first()
