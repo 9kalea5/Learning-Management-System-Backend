901e03fc242d5cb9509067df6f52e6133264443d
kaleab <alebachewkaleab@gmail.com>
2025-07-03 20:07:24 +0300

    Coupon Apply view

--- api/views.py
+++ api/views.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = api_serializer.MyTokenPairSerializer
