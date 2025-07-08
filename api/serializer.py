66645963f9e2e489d21c8e895e890db37b303dd7
kaleab <alebachewkaleab@gmail.com>
2025-07-08 10:39:36 +0300

    Course Seriailzer

--- api/serializer.py
+++ api/serializer.py
@@ -225,3 +225,10 @@ class CourseSerializer(serializers.ModelSerializer):
             "rating_count",
             "reviews",
         ]
+        def __init__(self, *args, **kwargs):
+            super(CourseSerializer, self).__init__(*args, **kwargs)
+            request = self.context.get("request")
+            if request and request.method == "POST":
+                self.Meta.depth = 0
+            else:
+                self.Meta.depth = 3
