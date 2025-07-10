328c781489757984f99a014e2711e625e631a42c
kaleab <alebachewkaleab@gmail.com>
2025-07-10 15:06:08 +0300

    Note List

--- api/urls.py
+++ api/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path("student/course-list/<user-id>/", api_views.StudentCourseListAPIView.as_view()),
     path("student/course-list/<user-id>/<enrolment_id>/", api_views.StudentCourseDetailAPIView.as_view()),
     path("student/course-completed/", api_views.StudentCourseCompletedCreateAPIView.as_view()),
-    path("student/course-note/", api_views.StudentNoteCreateAPIView.as_view()), 
+    path("student/course-note/<user_id>/<enrollment_id>/", api_views.StudentNoteCreateAPIView.as_view()), 
     path("student/course-note/", api_views.StudentNoteCreateAPIView.as_view()),
     path("student/course-note-detail/<user_id>/<enrollment_id>/<note_id>/", api_views.StudentNoteDetailAPIView.as_view()),
     path("student/rate-course/", api_views.StudentRateCourseAPIView.as_view()),
