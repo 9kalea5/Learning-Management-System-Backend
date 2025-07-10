8d08fc9289ce0c0f2b5d3df40c3d02c1c332975f
kaleab <alebachewkaleab@gmail.com>
2025-07-10 09:51:27 +0300

    Rating Views

--- api/urls.py
+++ api/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path("student/course-note/", api_views.StudentNoteCreateAPIView.as_view()), 
     path("student/course-note/", api_views.StudentNoteCreateAPIView.as_view()),
     path("student/course-note-detail/<user_id>/<enrollment_id>/<note_id>/", api_views.StudentNoteDetailAPIView.as_view()),
+    path("student/rate-course/", api_views.StudentRateCourseAPIView.as_view()),
 ]
